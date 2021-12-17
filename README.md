1b4b4ccf42fdc2ea1372acb6e73b39b002a22da7
Erysilva98 <erimilsonsilva98@gmail.com>
2021-12-16 22:12:01 -0300

    Consulta CEP

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Jquery-e-Ajax
-Introdução a biblioteca Jquery
+### Introdução a biblioteca Jquery 
+
+----
+#### Criando Interface para Cusulta Cep
+
+- Interface intregada a uma url para buscar informações de CEP
+
+- As informações são listdas em uma tabela
+- Projeto prático de estudo
+- Introdução á Biblioteca jquery e Ajax para requisição dos Cep
---
+++ js/main.js
@@ -0,0 +1,28 @@
+function consultaCep() {
+    $(".barra-progresso").show();
+    var cep = document.getElementById("cep").value;
+    var url =  "https://viacep.com.br/ws/" + cep + "/json/";
+    $.ajax ({
+        url: url,
+        type: "GET",
+        success: function(response){
+            // Lista as informações em no console ao retirar as // abaixo
+            // console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#ddd").html(response.ddd);   
+            $("#ibge").html(response.ibge);  
+            $("#siafi").html(response.siafi);  
+            $("#titulo-cep").html("CEP : " + response.cep); 
+            $(".cep").show();
+            $(".barra-progresso").hide();
+        }
+    })
+}
+
+$(function() {
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
